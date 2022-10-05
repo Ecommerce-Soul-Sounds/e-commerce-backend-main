74e0979388f4b9e10d83d1131905fa98b8a205ac
SquidicusJohnson <tristanballman@gmail.com>
2022-10-05 11:29:27 -0500

    refactor wishList controller and service

--- src/main/java/com/revature/controllers/WishlistController.java
+++ src/main/java/com/revature/controllers/WishlistController.java
@@ -1,88 +1,53 @@
 package com.revature.controllers;
 
 import com.revature.annotations.Authorized;
-import com.revature.exceptions.NoProductFoundException;
-import com.revature.exceptions.WishlistErrorException;
-import com.revature.models.Product;
 import com.revature.models.User;
 import com.revature.models.Wishlist;
 import com.revature.models.WishlistItem;
-import com.revature.services.ProductService;
-import com.revature.services.WishlistItemService;
 import com.revature.services.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/wishlist")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:3000" }, allowCredentials = "true")
 public class WishlistController {
 
-    @Autowired
-    private WishlistService wishlistService;
+	@Autowired
+	private WishlistService wishlistService;
 
-    @Autowired
-    private WishlistItemService wishlistItemService;
+	@Authorized
+	@GetMapping
+	public @ResponseBody Wishlist getUserWishlist(HttpSession session) {
+		User loggedInUser = (User) session.getAttribute("user");
+		return wishlistService.getWishlistById(loggedInUser.getWishlist().getId());
+	}
 
-    @Autowired
-    private ProductService productService;
+	@Authorized
+	@GetMapping("/items")
+	public @ResponseBody List<WishlistItem> getWishlistItems(HttpSession session) {
+		User loggedInUser = (User) session.getAttribute("user");
+		return wishlistService.getWishlistItemsByWishlistID(loggedInUser.getWishlist().getId());
+	}
 
-    @Authorized
-    @GetMapping
-    public @ResponseBody Wishlist getUserWishlist(HttpServletRequest request) {
-        User loggedInUser = (User) request.getSession().getAttribute("user");
+	@Authorized
+	@PostMapping("/add-item")
+	public @ResponseBody String addWishlistItem(@RequestBody int productId, HttpSession session) {
+		User loggedInUser = (User) session.getAttribute("user");
+		wishlistService.updateWishlist(loggedInUser.getWishlist());
+		Boolean success = wishlistService.addWishlistItem(loggedInUser.getWishlist(), productId);
+		return success ? "Item added to wishlist" : "Something went wrong, item was not added to wishlist." ;
+	}
 
-        return wishlistService.getWishlistById(loggedInUser.getWishlist().getId());
-    }
-
-    @Authorized
-    @GetMapping("/items")
-    public @ResponseBody List<WishlistItem> getWishlistItems(HttpServletRequest request) {
-        User loggedInUser = (User) request.getSession().getAttribute("user");
-
-        return wishlistItemService.getWishlistItemsByWishlistID(loggedInUser.getWishlist().getId());
-    }
-
-    @Authorized
-    @PostMapping("/add-item")
-    public @ResponseBody WishlistItem addWishlistItem(@RequestBody int productId, HttpServletRequest request) {
-        User loggedInUser = (User) request.getSession().getAttribute("user");
-
-        WishlistItem item = new WishlistItem();
-        item.setWishlist(loggedInUser.getWishlist());
-
-        Optional<Product> optional = productService.findById(productId);
-
-        if (optional.isPresent()) {
-            item.setProduct(optional.get());
-            wishlistService.updateWishlist(loggedInUser.getWishlist());
-            return wishlistItemService.addWishlistItem(item);
-        } else {
-            throw new NoProductFoundException("Product with that ID was not found.");
-        }
-    }
-
-    @Authorized
-    @DeleteMapping("/delete-item")
-    public @ResponseBody String deleteWishlistItem(@RequestBody int productId, HttpServletRequest request) {
-        User loggedInUser = (User) request.getSession().getAttribute("user");
-
-        WishlistItem item = new WishlistItem();
-        item.setWishlist(loggedInUser.getWishlist());
-
-        Optional<Product> optional = productService.findById(productId);
-
-        if (optional.isPresent()) {
-            item.setProduct(optional.get());
-            wishlistItemService.deleteWishlistItem(item);
-            wishlistService.updateWishlist(loggedInUser.getWishlist());
-            return "Item successfully removed from your wishlist.";
-        } else {
-            throw new NoProductFoundException("Product with that ID was not found.");
-        }
-    }
+	@Authorized
+	@DeleteMapping("/delete-item")
+	public @ResponseBody String deleteWishlistItem(@RequestBody int productId, HttpSession session) {
+		User loggedInUser = (User) session.getAttribute("user");
+		Boolean success = wishlistService.deleteWishlistItem(loggedInUser.getWishlist(), productId);
+		return success ? "Item successfully removed from your wishlist." : "Something went wrong, item could not be removed.";
+	}
 }
--- src/main/java/com/revature/services/WishlistService.java
+++ src/main/java/com/revature/services/WishlistService.java
@@ -1,12 +1,17 @@
 package com.revature.services;
 
+import com.revature.models.Product;
 import com.revature.models.Wishlist;
+import com.revature.models.WishlistItem;
+import com.revature.repositories.ProductRepository;
+import com.revature.repositories.WishlistItemRepository;
 import com.revature.repositories.WishlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WishlistService {
@@ -14,6 +19,12 @@ public class WishlistService {
     @Autowired
     private WishlistRepository wishlistRepo;
 
+    @Autowired
+    private WishlistItemRepository wishlistItemRepo;
+    
+    @Autowired
+    private ProductRepository productRepo;
+    
     public Wishlist addWishlist(Wishlist wishlist) {
         return wishlistRepo.save(wishlist);
     }
@@ -29,4 +40,37 @@ public class WishlistService {
     public void deleteWishlist(Wishlist wishlist) {
         wishlistRepo.delete(wishlist);
     }
+
+	public List<WishlistItem> getWishlistItemsByWishlistID(int id) {
+		return wishlistItemRepo.getWishlistItemsByWishlistID(id);
+	}
+
+	public boolean addWishlistItem(Wishlist wishlist, int productId) {
+		WishlistItem item = new WishlistItem();
+		
+		Optional<Product> product = productRepo.findById(productId);
+		
+		if (product.isPresent()) {
+			item.setProduct(product.get());
+			item.setWishlist(wishlist);
+			return wishlistItemRepo.save(item) != null;
+		}
+		
+		return false;
+	}
+
+	public boolean deleteWishlistItem(Wishlist wishlist, int productId) {
+		WishlistItem item = new WishlistItem();
+		
+		Optional<Product> product = productRepo.findById(productId);
+		
+		if (product.isPresent()) {
+			item.setProduct(product.get());
+			item.setWishlist(wishlist);
+			wishlistItemRepo.delete(item);
+			return true;
+		}
+		
+		return false;
+	}
 }
