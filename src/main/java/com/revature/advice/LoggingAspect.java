f54302c182efac1558859b474005463e72c4d1ae
joelmaridas <joel254@revature.net>
2022-10-05 13:36:05 -0500

    Made Changes to OrderController,OrderService and OrderTest

--- src/main/java/com/revature/advice/LoggingAspect.java
+++ src/main/java/com/revature/advice/LoggingAspect.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
 public class LoggingAspect {
@@ -22,7 +24,7 @@ public class LoggingAspect {
 	@Before(value="execution(* com.revature.controllers.*.*(..))")
 	public void logBeforeController(JoinPoint jp) throws JsonProcessingException {
 		log.info("Incoming request routed to - [ {} : {} ]", jp.getTarget().getClass().getName(), jp.getSignature().getName());
-		if (jp.getArgs().length > 0) {
+		if (jp.getArgs().length > 0 && !(jp.getArgs()[0] instanceof org.apache.catalina.session.StandardSessionFacade) ) {
 			String requestJSON = om.writeValueAsString(jp.getArgs()[0]);
 			log.info("Request body is: {}", requestJSON);
 		}
@@ -39,15 +41,15 @@ public class LoggingAspect {
 	}
 	
 	@Before(value="execution(* com.revature.repositories.*.*(..))")
-	public void logBeforeDAO(JoinPoint jp) throws JsonProcessingException {
+	public void logBeforeDAO(JoinPoint jp) {
 		log.debug("calling dao method - [ {} : {} ]: with args", jp.getTarget().getClass().getName(), jp.getSignature().getName());
 		for (Object arg : jp.getArgs()) {
-			log.debug("{} : {}", arg.getClass().getName(), om.writeValueAsString(arg));
+			log.debug("{} : {}", arg.getClass().getName(), arg.toString());
 		}
 	}
 	
 	@AfterReturning(value="execution(* com.revature.repositories.*.*(..))", returning = "result")
-	public void logAfterDAO(JoinPoint jp, Object result) throws JsonProcessingException {
-		log.debug("Exiting dao method - [ {} : {} ]: returning {}", jp.getTarget().getClass().getName(), jp.getSignature().getName(), om.writeValueAsString(result));
+	public void logAfterDAO(JoinPoint jp, Object result) {
+		log.debug("Exiting dao method - [ {} : {} ]: returning {}", jp.getTarget().getClass().getName(), jp.getSignature().getName(), result.toString());
 	}
 }
--- src/test/java/com/revature/servicesTest/WishlistServiceTest.java
+++ src/test/java/com/revature/servicesTest/WishlistServiceTest.java
@@ -1,6 +1,10 @@
 package com.revature.servicesTest;
 
+import com.revature.models.Product;
 import com.revature.models.Wishlist;
+import com.revature.models.WishlistItem;
+import com.revature.repositories.ProductRepository;
+import com.revature.repositories.WishlistItemRepository;
 import com.revature.repositories.WishlistRepository;
 import com.revature.services.WishlistService;
 import org.junit.jupiter.api.*;
@@ -11,10 +15,13 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,18 +29,32 @@ import static org.mockito.Mockito.when;
 class WishlistServiceTest {
     @Mock
     private static WishlistRepository mockWishlistDao;
+    
+    @Mock
+    private static WishlistItemRepository mockWishlistItemDao;
+    
+    @Mock
+    private static ProductRepository mockProductDao;
 
     @InjectMocks
     private static WishlistService wishlistService;
+    
     private static Wishlist wishlist1;
+    private static Product  product1;
+    private static WishlistItem item1;
 
+    
     @BeforeAll
     static void setupBeforeClass() {
         mockWishlistDao = Mockito.mock(WishlistRepository.class);
-
+        mockWishlistItemDao = Mockito.mock(WishlistItemRepository.class);
+        mockProductDao = Mockito.mock(ProductRepository.class);
+        
         wishlistService = new WishlistService();
 
         wishlist1 = new Wishlist(1, LocalDate.now());
+        product1 = new Product(1, 10, 100, "Instrument", "TrumpetsRUs", "A Trumpet", null, "Trumpet");
+        item1 = new WishlistItem(1, product1, wishlist1);
     }
 
     @Test
@@ -41,6 +62,9 @@ class WishlistServiceTest {
     @DisplayName("1. Mock validation Sanity Test")
     void checkMockInjection() {
         assertThat(mockWishlistDao).isNotNull();
+        assertThat(mockWishlistDao).isNotNull();
+        assertThat(mockWishlistItemDao).isNotNull();
+        assertThat(mockProductDao).isNotNull();
         assertThat(wishlistService).isNotNull();
     }
 
@@ -73,5 +97,39 @@ class WishlistServiceTest {
         when(mockWishlistDao.getWishlistById(1)).thenReturn(wishlist1);
 
         assertEquals(wishlist, wishlistService.getWishlistById(1));
+    }
+    
+    @Test
+    @Order(5)
+    @DisplayName("5. Test add to wishlist")
+    void testAddToWishlist() {	
+    	WishlistItem item = new WishlistItem();
+    	item.setWishlist(wishlist1);
+    	item.setProduct(product1);
+    	when(mockWishlistItemDao.save(item)).thenReturn(item1);
+    	when(mockProductDao.findById(1)).thenReturn(Optional.of(product1));
+    	
+    	assertTrue(wishlistService.addWishlistItem(wishlist1, 1));
+    }
+    
+    @Test
+    @Order(6)
+    @DisplayName("6. Test remove from wishlist")
+    void testRemoveFromWishlist() {
+    	WishlistItem item = new WishlistItem();
+    	item.setWishlist(wishlist1);
+    	item.setProduct(product1);
+    	when(mockProductDao.findById(1)).thenReturn(Optional.of(product1));
+    	assertTrue(wishlistService.deleteWishlistItem(wishlist1, 1));
+    }
+    
+    @Test
+    @Order(7)
+    @DisplayName("7. Test get all items for wishlist")
+    void testGetAllWishlistItemsByWishlist() {
+    	List<WishlistItem> list = new ArrayList<>();
+    	list.add(item1);
+    	when(mockWishlistItemDao.getWishlistItemsByWishlistID(1)).thenReturn(list);
+    	assertEquals(list , wishlistService.getWishlistItemsByWishlist(wishlist1));
     }
 }
